7d4685a7e011cad1d21ac8cb7efa7d1fd3528036
WeitgasserSimon <simon.weitgasser08@htl-salzburg.ac.at>
2024-12-11 07:42:37 +0100

    even better now

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -20,10 +20,6 @@ public class Manager : MonoBehaviour
         {
             Debug.Log($"Student {i}: {students[i].name}, Image: {students[i].studentImage}");
         }
-
-        // Restlicher Code...
-
-
         for (int row = 0; row < tableLayout.rows; row++)
         {
             for (int column = 0; column < tableLayout.columns; column++)
@@ -31,29 +27,12 @@ public class Manager : MonoBehaviour
                 Vector3 tablePosition = new Vector3(column * tableLayout.tableSpacing, 0, row * tableLayout.tableSpacing);
                 GameObject table = Instantiate(tablePrefab, tablePosition, Quaternion.identity, transform);
                 AssignStudentToChair(table);
-
-                 //Sessel platzieren
-                //Transform pos1 = table.transform.Find("pos1");
-                //Transform pos2 = table.transform.Find("pos2");
-
-                //if (pos1)
-                //{
-                //    GameObject chair1 = Instantiate(chairPrefab, pos1.position, pos1.rotation, table.transform);
-                //    AssignStudentToChair(chair1);
-                //}
-
-                //if (pos2)
-                //{
-                //    GameObject chair2 = Instantiate(chairPrefab, pos2.position, pos2.rotation, table.transform);
-                //    AssignStudentToChair(chair2);
-                //}
             }
         }
     }
 
     public void AssignStudentToChair(GameObject table)
     {
-        // Finde alle "StudentImage"-Objekte unter diesem Tisch
         Transform[] allTransforms = table.GetComponentsInChildren<Transform>();
         foreach (var transform in allTransforms)
         {
@@ -69,8 +48,6 @@ public class Manager : MonoBehaviour
                 studentIndex++;
 
                 Debug.Log($"Assigning student: {student.name} with image: {student.studentImage}");
-
-                // Überprüfe, ob eine Image-Komponente vorhanden ist
                 var imageComponent = transform.GetComponent<UnityEngine.UI.Image>();
                 if (imageComponent != null)
                 {
